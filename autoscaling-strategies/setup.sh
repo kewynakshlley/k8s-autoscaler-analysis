c26b71f2b7109a1bb23977c269b2a0329f918db7
Kewyn Akshlley <kewyn.akshlley@dcx.ufpb.br>
2020-11-06 15:21:11 -0300

    vpa bench config

--- autoscaling-strategies/setup.sh
+++ autoscaling-strategies/setup.sh
@@ -5,7 +5,7 @@ minikube start
 minikube addons enable metrics-server
 
 # install vpa
-./autoscaler/vertical-pod-autoscaler/hack/vpa-up.sh
+#./autoscaler/vertical-pod-autoscaler/hack/vpa-up.sh
 
 # create deployment, service and vpa
 # kubectl apply -f .
