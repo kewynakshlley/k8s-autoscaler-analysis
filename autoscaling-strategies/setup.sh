b30d4e336fdc44e2f0e1bb773e353eb5bb5370c7
Kewyn Akshlley <kewyn.akshlley@dcx.ufpb.br>
2020-11-13 08:00:40 -0300

    improving initial setup script

--- autoscaling-strategies/setup.sh
+++ autoscaling-strategies/setup.sh
@@ -3,9 +3,9 @@
 minikube stop
 minikube delete
 minikube start --kubernetes-version v1.19.3 --driver=virtualbox
-kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/download/v0.4.0/components.yaml
+kubectl apply -f vpa/components.yaml
 # install vpa
 vpa/autoscaler/vertical-pod-autoscaler/hack/vpa-up.sh
-
+kubectl apply -f vpa/manifests .
 # create deployment, service and vpa
 # kubectl apply -f .
