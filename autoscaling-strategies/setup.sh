3ea5b9c171ed16ba8f2c47d9b250026d95b8525e
Kewyn Akshlley <kewyn.akshlley@dcx.ufpb.br>
2020-11-16 08:24:41 -0300

    refactoring setup script

--- autoscaling-strategies/setup.sh
+++ autoscaling-strategies/setup.sh
@@ -5,7 +5,6 @@ minikube delete
 minikube start --kubernetes-version v1.19.3 --driver=virtualbox
 kubectl apply -f vpa/components.yaml
 # install vpa
-vpa/autoscaler/vertical-pod-autoscaler/hack/vpa-up.sh
-kubectl apply -f vpa/manifests .
+
 # create deployment, service and vpa
 # kubectl apply -f .
