660cdb2ca1e87d778cce0a6f780280e65be380bd
Kewyn Akshlley <kewyn.akshlley@dcx.ufpb.br>
2020-11-17 06:47:51 -0300

    getting service url dynamically

--- autoscaling-strategies/hpa/start-hpa-benchmark.sh
+++ autoscaling-strategies/hpa/start-hpa-benchmark.sh
@@ -1,32 +1,35 @@
 #!/bin/bash
 
-HOST=$1
-SERVICE_TIME='busy-wait/175'
+HOST=""
+SERVICE_TIME='/busy-wait/250'
 DURATION=2
 
 LOGS_DIR=logs
 STAGE_DIR=""
 SCENARIO_DIR=""
-SCENARIOS=(REQUEST_RATE_S1 REQUEST_RATE_S2 REQUEST_RATE_S3 REQUEST_RATE_S4)
+SCENARIOS=(REQUEST_RATE_S1 REQUEST_RATE_S2 REQUEST_RATE_S3 REQUEST_RATE_S4 REQUEST_RATE_S5)
 
 # Scenario 1
-REQUEST_RATE_S1=(1 1 2 4 8 4 2 1)
+REQUEST_RATE_S1=(2 2 4 6 8 6 4 2)
 
 # Scenario 2
-REQUEST_RATE_S2=(1 1 8 8 8 1 1 1)
+REQUEST_RATE_S2=(2 2 4 4 8 8 4 4)
 
 # Scenario 3
-REQUEST_RATE_S3=(1 1 8 8 1 1 8 8)
+REQUEST_RATE_S3=(2 2 8 8 8 2 2 2)
 
 # Scenario 4
-REQUEST_RATE_S4=(1 1 8 1 8 1 8 1)
+REQUEST_RATE_S4=(2 2 8 8 2 2 8 8)
+
+# Scenario 4
+REQUEST_RATE_S5=(2 2 8 2 8 2 8 2)
 
 function create_dir(){
     if [ ! -d "${1}" ]; then
         mkdir "${1}"
     fi
-    mkdir "${1}/node"
     mkdir "${1}/hpa"
+    mkdir "${1}/hpa-json"
     mkdir "${1}/events"
     mkdir "${1}/pods-requests"
 }
@@ -34,9 +37,9 @@ function create_dir(){
 function save_logs(){
     for value in {1..12}
     do
-        (kubectl get hpa slave-leech-hpa) >> "${SCENARIO_DIR}/${STAGE_DIR}/hpa/hpa-usage.log"
+        (kubectl get hpa busy-wait-hpa) >> "${SCENARIO_DIR}/${STAGE_DIR}/hpa/hpa-usage.log"
         (kubectl get pods -o json) > "${SCENARIO_DIR}/${STAGE_DIR}/pods-requests/pod-request-${value}.log"
-        (kubectl top nodes) >> "${SCENARIO_DIR}/${STAGE_DIR}/node/node-usage.log"
+        (kubectl get hpa busy-wait-hpa -o json) > "${SCENARIO_DIR}/${STAGE_DIR}/hpa-json/hpa-${value}.log"
         sleep 10;
     done
 }
@@ -44,7 +47,7 @@ function save_logs(){
 function info(){
     printf "\U1F680 Starting scenario ${1}. It will take 20 minutes...\n"
     printf "\U1F984 Saving HPA usage info..\n"
-    printf "\U1F984 Saving information about node usage..\n"
+    printf "\U1F984 Saving node info..\n"
     printf "\U1F984 Saving HPA events..\n"
 }
 
@@ -65,14 +68,18 @@ function start_experiment(){
             STAGE_DIR="stage-${INC}"
             create_dir "${SCENARIO_DIR}/${STAGE_DIR}"
 
+            (kubectl describe nodes minikube) > "${SCENARIO_DIR}/${STAGE_DIR}/node-info.log"
+            echo "REQUEST RATE = $REQ_RATE"
+            HOST=$(minikube service busy-wait-hpa --url)
             save_logs &
-            (hey -z ${DURATION}m -c ${REQ_RATE} -q 1 -m GET -T “application/x-www-form-urlencoded” ${HOST}${SERVICE_TIME}) > "${SCENARIO_DIR}/${STAGE_DIR}/hey=${INC}-$(date +%r).log"
-            (kubectl get hpa busy-wait-hpa -o json) > "${SCENARIO_DIR}/${STAGE_DIR}/events/events=${INC}.log"
+            (hey -disable-keepalive -z ${DURATION}m -c ${REQ_RATE} -q 1 -m GET -T “application/x-www-form-urlencoded” ${HOST}${SERVICE_TIME}) > "${SCENARIO_DIR}/${STAGE_DIR}/hey-info.log"
+            (kubectl get hpa busy-wait-hpa -o json) > "${SCENARIO_DIR}/${STAGE_DIR}/events/events-info.log"
 
             INC=$((INC+1))
         done
         printf "\U1F973  Scenario finished. The logs were saved.\n"
         S_AUX=$((S_AUX+1))
+        source restart-hpa.sh
     done
     echo "Experiment finished."
 }
